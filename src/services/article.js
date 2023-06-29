b8bbdd109ec17fa9838f92a5f4f47308ec9b46ac
Chris <116923904+moosefacts@users.noreply.github.com>
2023-06-29 10:23:06 -0700

    added article summary section and copy button functionality

--- src/services/article.js
+++ src/services/article.js
@@ -7,7 +7,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', 'rapidApiKey');
+            headers.set('X-RapidAPI-Key', 'c357e2865bmsh714e4e4b17ca593p14427fjsn069efbb52262');
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
